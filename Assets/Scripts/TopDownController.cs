1259450c66253ee7b246d2ffca0ab0a8bd29a9a7
MilkyQuartz <milkyquartz.2001@gmail.com>
2024-05-09 16:44:04 +0900

    New Input System 추가

--- Assets/Scripts/TopDownController.cs
+++ Assets/Scripts/TopDownController.cs
@@ -8,4 +8,13 @@ public class TopDownController : MonoBehaviour
     public event Action<Vector2> OnMoveEvent; // Action은 무조건 void만 반환, 아니면 Func
     public event Action<Vector2> OnLookEvent;
 
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction); // 없으면 말고 있으면 실행한다.
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
 }
---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    // 실제로 이동이 일어날 컴포넌트
+
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+    private Vector2 movementDirection = Vector2.zero;
+
+    // Awake는 주로 내 컴포넌트 안에서 끝나는거
+    private void Awake()
+    {
+        // controller랑 TopDownMovement랑 같은 GameObject에 있다라는 가정
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdate는 물리 업데이트 관련
+        // Rigidbody의 값을 바꾸니깐 FixedUpdate 사용
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody.velocity = direction; // velocity 공부하기
+    }
+}
